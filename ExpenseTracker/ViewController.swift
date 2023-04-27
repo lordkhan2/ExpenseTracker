fa2258402fea03e09210c99c46949cd36a892bca
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2023-04-27 17:16:14 +1000

    Initial Commit

---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Farhan Khan on 27/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
