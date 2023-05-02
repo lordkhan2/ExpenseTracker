9673e06047aed1cb0c933c6d8c9ae85503946433
Kawshik Roy <Kawshik.Roy@student.uts.edu.au>
2023-05-02 13:09:38 +1000

    Storyboarding Completed, except Settings and Alerts

---
+++ ExpenseTracker/Controller/AddExpenseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpense.swift
+//  ExpenseTracker
+//
+//  Created by Cube on 5/1/23.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ ExpenseTracker/Controller/ExpensesListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExpensesList.swift
+//  ExpenseTracker
+//
+//  Created by Cube on 5/1/23.
+//
+
+import UIKit
+
+class ExpensesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
