e57f32bba14d99f47eaf4f3838bbcc980608e61a
Yangru Guo <guoyangru@gmail.com>
2023-05-07 19:26:12 +1000

    - Changes have been made in the add expenses add image, by clicking the image view, the camera will show up - Changes to Setting Views (List of views with all settings items)

--- ExpenseTracker/Controller/AddExpenseViewController.swift
+++ ExpenseTracker/Controller/AddExpenseViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class AddExpenseViewController : UIViewController {
+class AddExpenseViewController : UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var amountTextField: UITextField!
@@ -26,6 +26,10 @@ class AddExpenseViewController : UIViewController {
         super.viewDidLoad()
         createDatepicker()
         // Do any additional setup after loading the view.
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        recieptImageView.isUserInteractionEnabled = true
+        recieptImageView.addGestureRecognizer(tapGestureRecognizer)
+        recieptImageView.backgroundColor = .systemGray
     }
     
     func createToolbar() -> UIToolbar {
@@ -66,5 +70,29 @@ class AddExpenseViewController : UIViewController {
         print("| EZPROGAMER \(id)| \(expenseDate)| \(expenseAmount)| \(expenseCategory)| \(expensePaymentType)| \(expenseDescription)| \(expenseRecieptImage)| \(expenseNotes)")
         
         db.insert(expenseDate: expenseDate, amount: expenseAmount, category: expenseCategory, paymentType: expensePaymentType, description: expenseDescription, imageLink: expenseRecieptImage, notes: expenseNotes)
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        // Your action
+        print("View Tapped")
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+}
+
+extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    func imagePickerControllerDidCancel(_ picker:UIImagePickerController){
+        picker.dismiss(animated:true, completion: nil)
+    }
+    func imagePickerController(_ picker:UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]){
+        picker.dismiss(animated: true,completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else{
+            return
+        }
     }
 }
--- ExpenseTracker/Controller/SettingsViewController.swift
+++ ExpenseTracker/Controller/SettingsViewController.swift
@@ -7,12 +7,138 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+struct Section{
+    let title: String
+    let options:[SettingsOptionType]
+}
 
+//enlist all types of button
+enum SettingsOptionType{
+    case staticCell(model:SettingsOption)
+}
+
+// To be added if more types of button are needed
+struct SettingsOption{
+    let title:String
+    let icon:UIImage?
+    let iconBGColor:UIColor
+    let handler:(() -> Void)
+}
+
+
+class SettingsViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var settingTableView: UITableView!
+    
+    var models = [Section]()
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        settingTableView.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.id)
+        configure()
     }
+    func configure(){
+        models.append(Section(title: "Expense Settings", options: [
+            .staticCell(model:SettingsOption(title: "Fixed Cost and Income", icon: UIImage(systemName:"banknote"), iconBGColor: .systemYellow){
+                //code to be added when user click the button
+                print("Test: First cell has been selected")
+            }),
+            .staticCell(model:SettingsOption(title: "Edit Categories", icon: UIImage(systemName:"pencil"), iconBGColor: .systemBlue){
+                //code to be added when user click the button
+            })
+        ])
+        )
+        
+        models.append(Section(title: "Graph Settings", options: [
+            .staticCell(model: SettingsOption(title: "Start Date of Month", icon: UIImage(systemName:"calendar"), iconBGColor: .systemGreen){
+                //code to be added when user click the button
+            }),
+            .staticCell(model: SettingsOption(title: "Start Date of Week", icon: UIImage(systemName:"calendar"), iconBGColor: .systemGreen){
+                //code to be added when user click the button
+            }),
+            .staticCell(model: SettingsOption(title: "Graph Settings", icon: UIImage(systemName:"chart.xyaxis.line"), iconBGColor: .systemGreen){
+                //code to be added when user click the button
+            })
+        ])
+        )
+        
+        models.append(Section(title: "General Settings", options: [
+            .staticCell(model: SettingsOption(title: "Language", icon: UIImage(systemName:"text.bubble"), iconBGColor: .systemBlue){
+                //code to be added when user click the button
+            }),
+            .staticCell(model: SettingsOption(title: "Currency", icon: UIImage(systemName:"dollarsign.arrow.circlepath"), iconBGColor: .systemRed){
+                //code to be added when user click the button
+            }),
+            .staticCell(model: SettingsOption(title: "Reminder", icon: UIImage(systemName:"exclamationmark.bubble"), iconBGColor: .systemPink){
+                //code to be added when user click the button
+            })
+        ])
+        )
+        
+        models.append(Section(title: "Reporting Settings", options: [
+            .staticCell(model: SettingsOption(title: "Yearly Report", icon: UIImage(systemName:"newspaper"), iconBGColor: .systemBlue){
+                //code to be added when user click the button
+            }),
+            .staticCell(model: SettingsOption(title: "Yearly Category-wise Report", icon: UIImage(systemName:"newspaper"), iconBGColor: .systemBlue){
+                //code to be added when user click the button
+            }),
+            .staticCell(model: SettingsOption(title: "Yearly Reporting Yearly Category-wise Report", icon: UIImage(systemName:"calendar.badge.clock"), iconBGColor: .systemBlue){
+                //code to be added when user click the button
+            }),
+            .staticCell(model: SettingsOption(title: "Yearly Category-wise Report Yearly Category-wise Report", icon: UIImage(systemName:"calendar.badge.clock"), iconBGColor: .systemGreen){
+                //code to be added when user click the button
+            })
+        ])
+        )
+        
+        models.append(Section(title: "App Settings", options: [
+            .staticCell(model: SettingsOption(title: "FAQ", icon: UIImage(systemName:"person.fill.questionmark"), iconBGColor: .systemBlue){
+                //code to be added when user click the button
+            }),
+            .staticCell(model: SettingsOption(title: "About the App", icon: UIImage(systemName:"app"), iconBGColor: .systemGreen){
+                //code to be added when user click the button
+            })
+        ])
+        )
+        
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = models[section]
+        return section.title
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int{
+        return models[section].options.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section].options[indexPath.row]
+        //configure based on different type of cells
+        switch model.self{
+        case .staticCell(let model):
+            guard let cell  = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.id, for:indexPath) as? SettingTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
+        }
 
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let type = models[indexPath.section].options[indexPath.row]
+        //to be added based on different types of cells
+        switch type.self{
+        case .staticCell(let model):
+            model.handler()
+        }
+    }
 
 }
