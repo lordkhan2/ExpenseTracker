c2a47ef76b067e1100ee91c9dc1fa1c03704d0f3
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2023-05-12 20:22:44 +1000

    Chart Library install and pie chart implemented

--- ExpenseTracker/Controller/ViewController.swift
+++ ExpenseTracker/Controller/ViewController.swift
@@ -6,12 +6,47 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import Charts
+class ViewController: UIViewController, ChartViewDelegate {
+    var pieChart = PieChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pieChart.frame = CGRect(x:0, y:0, width:self.view.frame.size.width, height: self.view.frame.size.width)
+        pieChart.center = view.center
+        view.addSubview(pieChart)
+        
+        
+        var entries = [ChartDataEntry]()
+//        for x in 0..<10 {
+//            entries.append(ChartDataEntry(x: Double(x), y: Double(x)))
+//        }
+        let expenseData = [
+            (0, 1000.0),
+            (1, 500.0),
+            (2, 300.0),
+            (3, 800.0),
+            (4, 900.0),
+            // Add more expense categories and amounts as needed
+        ]
+        for (number, amount) in expenseData {
+            let entry = ChartDataEntry(x: Double(number), y: Double(amount))
+            entries.append(entry)
+        }
+   
+        let set = PieChartDataSet(entries: entries)
+        
+        set.colors = ChartColorTemplates.material()
+        let data = PieChartData(dataSet: set)
+      
+        set.label = "Monthly Expense Breakdown"
+        pieChart.data = data
+       
+        
     }
 
 
